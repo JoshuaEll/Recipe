bfa2bd62573411edffaf2762bfc8915239435409
JoshuaEll <jbq715@my.utsa.edu>
2022-08-10 15:00:53 -0500

    added scroll state & query change functions

--- app/src/main/java/com/example/recipe/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/recipe/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -2,6 +2,7 @@ package com.example.recipe.presentation.ui.recipe_list
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.recipe.domain.model.Recipe
@@ -24,17 +25,20 @@ constructor(
 
     //mutable state for the user query to stop losing the input of the user
     val query = mutableStateOf("")
+    val selectedCategory: MutableState<FoodCategory?> = mutableStateOf(null)
+    var categoryScrollPosition: Int = 0
+
     init {
-        newSearch("Chicken")
+        newSearch()
     }
 
     //will be changed later to allow user searching
-    fun newSearch(query: String){
+    fun newSearch(){
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = query,
+                query = query.value,
             )
             recipes.value = result
         }
@@ -46,4 +50,14 @@ constructor(
         this.query.value =  query
     }
 
+    // changes the query to whatever chip was clicked
+    fun onSelectedCategoryChanged(category: String){
+        val newCategory = getFoodCategory(category)
+        selectedCategory.value = newCategory
+        onQueryChanged(category)
+    }
+
+    fun onChangeCategoryScrollPosition(position: Int){
+        categoryScrollPosition = position
+    }
 }
