45fa4aecf49d555385a2f43121359365d40d8601
JoshuaEll <jbq715@my.utsa.edu>
2022-08-11 20:56:57 -0500

    created SearchBar composable

---
+++ app/src/main/java/com/example/recipe/presentation/components/SearchAppBar.kt
@@ -0,0 +1,108 @@
+package com.example.recipe.presentation.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.recipe.presentation.ui.recipe_list.FoodCategory
+import com.example.recipe.presentation.ui.recipe_list.getAllFoodCategories
+import kotlinx.coroutines.launch
+
+@SuppressLint("CoroutineCreationDuringComposition")
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchAppBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+    scrollPosition: Int,
+    selectedCategory: FoodCategory?,
+    onSelectedCategoryChanged: (String) -> Unit,
+    onChangeCategoryScrollPosition: (Int) -> Unit,
+){
+    val keyboardController = LocalSoftwareKeyboardController.current
+    // since ScrollableRow does not work I have to work around it
+    val scrollStatus = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = Color.White,
+        elevation = 8.dp,
+    ){
+        Column {
+            Row{
+                //text field to let user enter stuff
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(8.dp),
+                    value = query,
+                    onValueChange = {
+                        onQueryChanged(it)
+                    },
+                    label = {
+                        Text(text = "Search")
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    ),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Filled.Search, contentDescription = "")
+                    },
+                    keyboardActions = KeyboardActions(onSearch = {
+                        onExecuteSearch()
+                        //IMEActions didn't work
+                        keyboardController?.hide()
+                    }),
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = MaterialTheme.colors.surface
+                    )
+                )
+            }
+
+            // ScrollableRow does not exist anymore, so I just use a regular row with scrolling turned on
+            Row(modifier = Modifier
+                .horizontalScroll(scrollStatus)
+                .fillMaxWidth()
+                .padding(start = 8.dp, bottom = 8.dp)){
+                coroutineScope.launch {
+                    scrollStatus.scrollTo(scrollPosition)
+                }
+                for(category in getAllFoodCategories()){
+                    // implement the chip
+                    FoodCategoryChip(
+                        category = category.value,
+                        isSelected =  selectedCategory == category,
+                        onSelectedCategoryChanged = {
+                            onSelectedCategoryChanged(it)
+                            onChangeCategoryScrollPosition(scrollStatus.value)
+                        },
+                        onExecuteSearch = {
+                            onExecuteSearch()
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
